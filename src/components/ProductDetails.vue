36971f708fdb9cbcb1da35ac2605152df41e0c77
Ilya <ildorf@list.ru>
2024-02-05 20:44:52 +0700

    first commit

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <img :src="product.img" alt="" />
+    <h3>Название продукта: {{ product.name }}</h3>
+    <p>Форматированная цена: {{ result.formattedPrice }}</p>
+    <p v-if="product.available">Available</p>
+    <p v-else="product.available">Out of stock</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      product: {
+        img: require("@/assets/img/1.jpg"),
+        name: "Миди клавиатура",
+        price: "1000",
+        available: false,
+      },
+    };
+  },
+  computed: {
+    result() {
+      const formattedPrice = "$" + this.product.price;
+      return { formattedPrice };
+    },
+  },
+};
+</script>
+
+<style scoped>
+img {
+  width: 200px;
+}
+</style>
